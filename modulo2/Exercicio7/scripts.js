dcc011763cfc82894fdacdcdc0ddcf7c32c436fa
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-07-28 15:19:32 -0300

    Exercicio 7 Realizado

--- modulo2/Exercicio7/scripts.js
+++ modulo2/Exercicio7/scripts.js
@@ -1,7 +1,17 @@
 // Duplique o menu e adicione ele em copy
-
+const menu = document.querySelector('.menu')
+const menuClonado = menu.cloneNode(true)
+const contato = document.querySelector('.contato')
+contato.appendChild(menuClonado)
 // Selecione o primeiro DT da dl de Faq
+const primeiroDt = document.querySelector('.faq dl dt')
+console.log(primeiroDt)
 
 // Selecione o DD referente ao primeiro DT
-
+const ddReferent = primeiroDt.nextElementSibling
+console.log(ddReferent)
 // Substitua o conteúdo html de .faq pelo de .animais
+const faq = document.querySelector('.faq')
+const animais = document.querySelector('.animais')
+
+faq.innerHTML = animais.innerHTML
