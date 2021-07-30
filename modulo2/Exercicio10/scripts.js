e54bf8e333f23d68e55d4b98d401a2f80fe35eb3
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-07-30 12:45:01 -0300

    Exercicio 10 Realizado, ultimo desafio

---
+++ modulo2/Exercicio10/scripts.js
@@ -0,0 +1,65 @@
+function initAnimationScroll() {
+    const sections = document.querySelectorAll('.js-scroll')
+    const windowMetade = window.innerHeight * 0.5
+    
+    function animationScroll() {
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top
+            const isSectionVisible = (sectionTop - windowMetade) < 0
+            if(isSectionVisible) {
+                section.classList.add('ativo')
+            } else {
+                section.classList.remove('ativo')
+            }
+        })
+    }
+    
+    animationScroll()
+    
+    window.addEventListener('scroll', animationScroll)
+}
+
+
+
+
+//Desafio do exercicio 9 - accordionList
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+    
+    function activeAccordion() {
+        this.classList.toggle('ativo')
+        this.nextElementSibling.classList.toggle('ativo')
+    }
+    
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion)
+    })
+}
+initAccordion()
+
+
+
+
+// Desafio do exercicio 8 -> TabMENU
+function initTabNav() {
+    const listAnimals = document.querySelectorAll('.js-tabmenu li')
+    const sectionAnimals = document.querySelectorAll('.js-tabcontent section')
+    if(sectionAnimals.length && listAnimals.length) {
+        sectionAnimals[0].classList.add('ativo')
+
+        listAnimals.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                activeTab(index)
+            })
+        })
+        function activeTab(index) {
+            sectionAnimals.forEach((section) => {
+                section.classList.remove('ativo')
+            })
+            sectionAnimals[index].classList.add('ativo')
+        }
+    }
+}
+initTabNav()
