5764adbb4705279692f0d62d570a6185cff3c950
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-07-30 11:42:34 -0300

    Exercicio 8 Realizado

---
+++ modulo2/exercicio8/scripts.js
@@ -0,0 +1,18 @@
+const listAnimals = document.querySelectorAll('.js-tabmenu li')
+const sectionAnimals = document.querySelectorAll('.js-tabcontent section')
+if(sectionAnimals.length && listAnimals.length) {
+    sectionAnimals[0].classList.add('ativo')
+
+    function activeTab(index) {
+        sectionAnimals.forEach((section) => {
+            section.classList.remove('ativo')
+        })
+        sectionAnimals[index].classList.add('ativo')
+    }
+
+    listAnimals.forEach((item, index) => {
+        item.addEventListener('click', function() {
+            activeTab(index)
+        })
+    })
+}
