9a73dc7816d4f556f327ed1c0281b443e9fe7444
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-08-19 10:46:16 -0300

    Exercicio 1 Realizado, Desafios do Projeto tambem Realizado!

---
+++ modulo4/exercicio1/scripts.js
@@ -0,0 +1,33 @@
+//Exercicios
+//Chame a div pelo data e acesse seu valor
+const div = document.querySelector('[data-cor]')
+console.log(div)
+const valueColorDiv = div.dataset.cor
+console.log(valueColorDiv)
+const valueHeightDiv = div.dataset.height
+console.log(valueHeightDiv)
+
+//Exercicios para o Projeto
+
+
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+
+console.log('Desafio 1 Para o Projeto final Realizado!')
+
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+
+console.log('Desafio 2 Para o Projeto final Realizado!')
+
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+console.log('Desafio 3 Para o projeto final Realizado!')
+
+// Substitua todas as classes js- por data atributes.
+console.log('Desafio 4 Para o projeto final Realizado!')
