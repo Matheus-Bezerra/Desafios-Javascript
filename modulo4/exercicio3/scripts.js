e2428673b7f8c7d200eb8f922d89714dd5e402da
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-08-22 13:55:15 -0300

    Exercicio 3 Realizado sobre a criação de Modal

--- modulo4/exercicio3/scripts.js
+++ modulo4/exercicio3/scripts.js
@@ -1,5 +1,8 @@
 // Realize um modal no arquivo do projeto Principal
 console.log('Modal foi Adicionada no Projeto Principal\n')
-// Explique a lógica Realizada para realizar A modal
 
-console.log('Selecionei os botões de Abrir e fechar, tambem foi selecionado a seção Inteira da Modal, após isso adicione um evento de Click par ao botão de Abrir e Fechar e em seus callback, vai ser o evento prevenido e adicione/remove(toggle) uma classe ativa na seção Modal e para finalizar vai ter um evento de click na seção Modal onde seu callback vai verificar se onde clicou é o this!')
+// Explique a lógica Realizada para realizar A modal
+const explanation = document.write('Explicação foi realizada no console de como foi feito a modal do projeto, já o desafio está no projeto final!')
+
+
+console.log('Selecionei os botões de Abrir e fechar, tambem foi selecionado a seção Inteira da Modal, após isso adicione um evento de Click par ao botão de Abrir e Fechar e em seus callback, vai ser o evento prevenido e adicione/remove(toggle) uma classe ativa na seção Modal e para finalizar vai ter um evento de click na seção Modal onde seu callback vai verificar se onde clicou é o this!, se for vai executar a função do botão abrir/fechar')
