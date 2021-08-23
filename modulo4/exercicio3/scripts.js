e57ded7048bf1f4633ddcd03d665a8e5781bbbe3
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-08-23 14:44:21 -0300

    Exercicio 3 Explicado como foi feito no projeto final

--- modulo4/exercicio3/scripts.js
+++ modulo4/exercicio3/scripts.js
@@ -2,4 +2,4 @@
 console.log('Modal foi Adicionada no Projeto Principal\n')
 
 // Explique a lógica Realizada para realizar A modal
-const explanation = document.write('Selecionei os botões de Abrir e fechar, tambem foi selecionado a seção Inteira da Modal, após isso adicione um evento de Click par ao botão de Abrir e Fechar e em seus callback, vai ser o evento prevenido e adicione/remove(toggle) uma classe ativa na seção Modal e para finalizar vai ter um evento de click na seção Modal onde seu callback vai verificar se onde clicou é o this!, se for vai executar a função do botão abrir/fechar.\nPROJETO ESTÁ REALIZADO NO ARQUIVO PROJETO FINAL!')
+const explanation = document.write('Selecionei os botões de Abrir e fechar, tambem foi selecionado a seção Inteira da Modal, após isso adicione um evento de Click par ao botão de Abrir e Fechar e em seus callback, vai ser o evento prevenido e adicione/remove(toggle) uma classe ativa na seção Modal e para finalizar vai ter um evento de click na seção Modal onde seu callback vai verificar se onde clicou é o this!, se for vai executar a função do botão abrir/fechar.<br>PROJETO ESTÁ REALIZADO NO ARQUIVO PROJETO FINAL!')
