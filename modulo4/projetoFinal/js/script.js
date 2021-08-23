a930618c3f2c0cb210c1b25c35b1b2f2d3f9cb73
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-08-23 14:54:50 -0300

    Projeto final para realização dos exercicios dos efeitos do Dom

--- modulo4/projetoFinal/js/script.js
+++ modulo4/projetoFinal/js/script.js
@@ -2,9 +2,14 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltips.js'
+import initDropdownMenu from './modules/dropdownMenu.js'
+
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal()
+initTooltip()
+initDropdownMenu()
