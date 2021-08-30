0e3ab08000c562efccf4bed0f9aa1f7c992005a2
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-08-30 11:22:21 -0300

    Exercicio 4 Realizado

---
+++ modulo5/exercicio4/scripts.js
@@ -0,0 +1,14 @@
+//crie um arquivo Json com um tema de criação de curso e sincronize com ele aqui e percorra sobre as matérias, aulas e minutos dele
+
+
+function aulas() {
+    fetch('./curso.json')
+    .then(r => r.json())
+    .then(json => {
+        json.forEach(json => {
+            console.log(json.id, json.aula, json.min)
+        });
+    })
+}
+
+aulas()
