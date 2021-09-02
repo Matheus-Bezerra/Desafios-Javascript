8582c307d1f2d6ebcb3b7dd2512b44a9053f5273
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-09-02 09:42:44 -0300

    Exercicio 8 Realizado, pegando valor do bitcoin e aplicando em contato em tempo real através de uma api

--- modulo5/exercicio8/scripts.js
+++ modulo5/exercicio8/scripts.js
@@ -1,3 +1,4 @@
 const body = document.querySelector('body')
 const title = document.createElement('h1')
-title.innerText = ''
+title.innerText = 'Bitcoin adicionado na parte do contato no Projeto final através de um valor atualizado em tempo real em uma Api'
+body.appendChild(title)
