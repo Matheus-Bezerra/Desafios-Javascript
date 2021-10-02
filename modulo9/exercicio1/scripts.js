8219203eff6b6015af780673cef541e1542e4fae
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-10-02 11:41:19 -0300

    Exercicio 1 sobre linhas de comando Realizada

---
+++ modulo9/exercicio1/scripts.js
@@ -0,0 +1,23 @@
+//Linha de comando! -> Interação com o computador através de texto(comandos)
+
+/*                          MOVIMENTAR:
+    cd sites -> Move para a pasta sites!
+    cd .. -> Move para a pasta anterior!
+    cd ~/Desktop/sites -> Move para a pasta sites dentro de Desktop!
+    cd ~ -> Move para o diretório prncipal do sistema / home!
+    clear -> Limpa a tela ou no caso o terminal!
+*/
+
+/*                          Lista e Criar 
+    ls -> lista diretórios e arquivos
+    ls -all -> lista diretórios, arquivos, invisíveis e detalhes
+    mkdir site -> criar o diretório site
+    touch index.html -> Cria o arquivo index.html!
+*/
+
+/*                          Remover 
+    rm index.html -> Remover arquivo index.html!
+    rm -r site -> remover o diretório site e todos os arquivos dentro dele
+    setas pra cima / baixo -> Acessa comandos anteriores
+    tab -> Auto complete o comando
+*/
